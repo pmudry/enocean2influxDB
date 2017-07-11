0e78c2f1cbe3bfe568329a844337203cc5c8ad66
Pierre-André Mudry <pmudry@gmail.com>
2017-07-11 11:50:53 +0200

    Blab

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,6 @@ import org.opencean.core.EnoceanSerialConnector;
 import org.opencean.core.address.EnoceanId;
 import org.opencean.core.common.ProtocolConnector;
 import org.opencean.core.packets.BasicPacket;
-import org.opencean.core.packets.QueryIdCommand;
 import org.opencean.core.packets.RadioPacket4BS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +17,13 @@ public class Application {
 	private static Logger logger = LoggerFactory.getLogger(Application.class);
 
 	Application(String port) {
-		logger.info("starting..");
 		ProtocolConnector connector = new EnoceanSerialConnector();
 		connector.connect(port);
 		ESP3Host esp3Host = new ESP3Host(connector);
-		BasicPacket packet = new QueryIdCommand();
-		esp3Host.sendRadio(packet);
+		
+//		BasicPacket packet = new QueryIdCommand();
+//		esp3Host.sendRadio(packet);
+
 		esp3Host.start();
 
 		esp3Host.addListener(new EnoceanReceiver() {
--- src/main/java/utils/Sensors.java
+++ src/main/java/utils/Sensors.java
@@ -10,7 +10,7 @@ public class Sensors {
 	static final byte[] therm1 = { (byte) 0x01, (byte) 0x9b, (byte) 0xdc, (byte) 0x47 };
 	static final byte[] therm2 = { (byte) 0x01, (byte) 0x82, (byte) 0x3a, (byte) 0xeb };
 	
-	public static HashMap<EnoceanId, STM330> sensors = new HashMap();
+	public static HashMap<EnoceanId, STM330> sensors = new HashMap<EnoceanId, STM330>();
 	
 	static {
 		sensors.put(new EnoceanId(therm1), new STM330("bureau"));
